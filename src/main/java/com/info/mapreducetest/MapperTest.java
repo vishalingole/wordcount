a13c94faa9dc22a05e3ade9bf17ffea0489a5c89
vishalingole <vishal.ingole3@gmail.com>
2016-11-18 21:34:04 +0530

    Update MapperTest.java

--- src/main/java/com/info/mapreducetest/MapperTest.java
+++ src/main/java/com/info/mapreducetest/MapperTest.java
@@ -1,20 +1,21 @@
-package com.info.mapreducetest;
+package com.info.mapreducetest; // Imported package
 
-import java.io.IOException;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException; //Imported hadoop java libraries
+import org.apache.hadoop.io.IntWritable; //Imported hadoop java libraries
+import org.apache.hadoop.io.LongWritable; //Imported hadoop java libraries
+import org.apache.hadoop.io.Text; //Imported hadoop java libraries
+import org.apache.hadoop.mapreduce.Mapper; //Imported hadoop java libraries
 
 public class MapperTest extends Mapper<LongWritable,Text,Text,IntWritable>{
-    IntWritable one=new IntWritable(1);
+    // Here MappterTest is a class that extends Mapper class with the two input parameteds that is "LongWritable,Text" & two otput parameters i.e "Text,IntWritable"
+    IntWritable one=new IntWritable(1); //Created Object of intwritable class
     
     @Override
-    public void map(LongWritable key,Text value, Context context) throws IOException, InterruptedException{
+    public void map(LongWritable key,Text value, Context context) throws IOException, InterruptedException{ //Here we override map method of mapper class
         
-        String[] line=value.toString().split(" ");
-        for(String word:line){
-            context.write(new Text(word.toLowerCase()), one);
+        String[] line=value.toString().split(" "); //All the words divied by space passed in split(" ")
+        for(String word:line){ //For to get all the words founding after split file with space 
+            context.write(new Text(word.toLowerCase()), one); // Here we creates pair of key,value to pass it to reducer
         }
         
     }
